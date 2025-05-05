c39727ff83b824af6fb4fc3a7254458851d4bc72
socratest3 <socratest3@nycstudents.net>
2025-05-05 18:58:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+        FrogSimulation sim = new FrogSimulation(24, 5);
+        FrogSimulation lim = new FrogSimulation(24, 4);
+        FrogSimulation dim = new FrogSimulation(31, 3);
+        FrogSimulation kim = new FrogSimulation(18, 5);
+System.out.println(sim.runSimulations(4));
+    }
+}
